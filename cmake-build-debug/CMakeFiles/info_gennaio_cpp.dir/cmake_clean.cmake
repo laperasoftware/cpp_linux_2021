54da970621aa91169e9425afc3366c884fa16b0a
Famiglia <monner.maggots@gmail.com>
2021-01-28 12:10:52 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/info_gennaio_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/info_gennaio_cpp.dir/main.cpp.o"
+  "info_gennaio_cpp"
+  "info_gennaio_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/info_gennaio_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
